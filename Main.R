b82c4e7172898fc8c7bf36294b32f6e83695d36d
jirehsc <101620350+jirehsc@users.noreply.github.com>
2025-12-13 14:48:15 +0800

    add

--- Main.R
+++ Main.R
@@ -7,6 +7,24 @@ summary(metadata)
 
 #install/loading packages
 
+library(tidyverse)
+library(dplyr)
+library(dbplyr)
+library(ggplot2)
+library(pheatmap)
+library(car)
+library(emmeans)
+library(lme4)
+library(lmerTest)
+library(ggpubr)
+library(rstatix)
+library(janitor)
+
+# Data Pipelining and Cleaning
+################################
+
+#hello world
+
 
 
 
